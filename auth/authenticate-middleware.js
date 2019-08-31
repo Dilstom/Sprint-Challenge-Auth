cf851843e6e11bfd0ec112a5de45b446c2b6f538
Dil Sombayeva <dilyasom@gmail.com>
2019-08-31 10:15:42 -0700

    add middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,26 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+ const secret = secrets.jwtSecret;
+ const token = req.headers.authorization;
+
+ if (token) {
+  jwt.verify(token, secret, (err, decodedToken) => {
+   if (err) {
+    res.status(401).json({ you: 'shall not pass!' });
+   } else {
+    req.user = {
+     username: decodedToken.uusername,
+    };
+    req.decodedJwt = decodedToken;
+    next();
+   }
+  });
+ } else {
+  res.status(401).json({ message: 'Login first!' });
+ }
 };
